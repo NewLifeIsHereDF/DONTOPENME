3da892ed0a998b50bdbcac405673e8f5f0b63188
NewLifeIsHereDF <speedy250536@gmail.com>
2025-01-11 14:24:41 +0100

    Update openme.bat

--- DontOpenMe/openme.bat
+++ DontOpenMe/openme.bat
@@ -1,20 +1,18 @@
 @echo off
 
-:: Vérifier les privilèges administratifs
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
-net user "DONT OPEN ME" /delete
+net user "DONT OPEN ME" /delete >nul 2>&1
 
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /f >nul 2>&1
 
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul 2>&1
 
 set desktop=%userprofile%\Desktop
 del /f /q "%desktop%\DONT_OPEN_ME_IS_HERE_*.txt" >nul 2>&1
@@ -24,13 +22,13 @@ del /f /q "%desktop%\DONT_OPEN_ME_RED.bmp" >nul 2>&1
 set startup_folder=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 del /f /q "%startup_folder%\DONT_OPEN_ME_*.bat" >nul 2>&1
 
-reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v Scancode Map /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v Scancode Map /f >nul 2>&1
+
+shutdown /a >nul 2>&1
 
 taskkill /im cmd.exe /f >nul 2>&1
 taskkill /im powershell.exe /f >nul 2>&1
 
-shutdown /a >nul 2>&1
-
-echo Les modifications apportées par "DONT OPEN ME" ont été désactivées dans la mesure du possible.
+echo Les modifications ont été annulées avec succès. Le système est revenu à la normale.
 pause
-exit 
+exit
